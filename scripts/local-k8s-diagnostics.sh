257bd2106ddccd33215ff64b6efe87fc889d2ab6
Roberto Estevão <robertode@microsoft.com>
2024-07-31 15:48:47 +0000

    Add all diagnostics back

--- scripts/local-k8s-diagnostics.sh
+++ scripts/local-k8s-diagnostics.sh
@@ -7,37 +7,37 @@ which kubectl
 
 echo "Cluster pods:"
 kubectl get pods
-# kubectl get pods -o yaml
+kubectl get pods -o yaml
 
-# echo "Docker images:"
-# docker images
+echo "Docker images:"
+docker images
 
-# echo "REST API description:"
-# kubectl describe deployment terravibes-rest-api
+echo "REST API description:"
+kubectl describe deployment terravibes-rest-api
 
-# echo "Orchestrator description:"
-# kubectl describe deployment terravibes-orchestrator
+echo "Orchestrator description:"
+kubectl describe deployment terravibes-orchestrator
 
-# echo "Worker description:"
-# kubectl describe deployment terravibes-worker
+echo "Worker description:"
+kubectl describe deployment terravibes-worker
 
-# echo "Cache description:"
-# kubectl describe deployment terravibes-cache
+echo "Cache description:"
+kubectl describe deployment terravibes-cache
 
-# echo "REST API logs:"
-# kubectl logs -l app=terravibes-rest-api --all-containers=true --tail=-1
+echo "REST API logs:"
+kubectl logs -l app=terravibes-rest-api --all-containers=true --tail=-1
 
-# echo "Orchestrator logs:"
-# kubectl logs -l app=terravibes-orchestrator --all-containers=true --tail=-1
+echo "Orchestrator logs:"
+kubectl logs -l app=terravibes-orchestrator --all-containers=true --tail=-1
 
-# echo "Worker logs:"
-# kubectl logs -l app=terravibes-worker --max-log-requests=8 --all-containers=true --tail=-1
+echo "Worker logs:"
+kubectl logs -l app=terravibes-worker --max-log-requests=8 --all-containers=true --tail=-1
 
-# echo "Cache logs:"
-# kubectl logs -l app=terravibes-cache --all-containers=true --tail=-1
+echo "Cache logs:"
+kubectl logs -l app=terravibes-cache --all-containers=true --tail=-1
 
-# echo "Data Ops logs:"
-# kubectl logs -l app=terravibes-data-ops --all-containers=true --tail=-1
+echo "Data Ops logs:"
+kubectl logs -l app=terravibes-data-ops --all-containers=true --tail=-1
 
-# echo "Kubernetes logs:"
-# docker ps | egrep 'k3d-farmvibes-ai-.*-0' | awk '{ print $1 }' | xargs docker logs
+echo "Kubernetes logs:"
+docker ps | egrep 'k3d-farmvibes-ai-.*-0' | awk '{ print $1 }' | xargs docker logs
