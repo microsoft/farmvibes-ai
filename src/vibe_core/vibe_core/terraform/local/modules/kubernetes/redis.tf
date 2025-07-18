ba52564bc6cbef8f288471acd48536763a1ebad0
Krishna Lodha <47075664+krishnaglodha@users.noreply.github.com>
2025-07-18 10:55:54 +0530

    fixed terraform and sets as per new standards

--- src/vibe_core/vibe_core/terraform/local/modules/kubernetes/redis.tf
+++ src/vibe_core/vibe_core/terraform/local/modules/kubernetes/redis.tf
@@ -8,25 +8,24 @@ resource "helm_release" "redis" {
   chart      = "redis"
   namespace  = var.namespace
 
-  set {
-    name  = "commonConfiguration"
-    value = "appendonly no"
-  }
-
-  set {
-    name  = "master.containerPort"
-    value = "6379"
-  }
-
-  set {
-    name  = "image.tag"
-    value = var.redis_image_tag
-  }
-
-  set {
-    name  = "replica.replicaCount"
-    value = "0"
-  }
+  set = [
+    {
+      name  = "commonConfiguration"
+      value = "appendonly no"
+    },
+    {
+      name  = "master.containerPort"
+      value = "6379"
+    },
+    {
+      name  = "image.tag"
+      value = var.redis_image_tag
+    },
+    {
+      name  = "replica.replicaCount"
+      value = "0"
+    }
+  ]
 }
 
 data "kubernetes_service" "redis" {
